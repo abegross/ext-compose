b52fb1dab76fe240e4ce855c57ef728fc879bbd3
First Last <ekaunt@mail.com>
2019-08-30 03:00:47 -0400

    updated dotfiles

--- compose_sequence_generator.py
+++ compose_sequence_generator.py
@@ -4,18 +4,18 @@ import unicodedata, re
 # the regular abcs or wtvr that will be typed out
 # each letter in 'regular' corresponds to the respective letter in 'composed'
 # regular = "0123456789"
-regular = ["ha","hi","hu","he","ho","ra","ri","ru","re","ro"]
+regular = ["~~"]
 
 # the letter that compose spits out
 # composed = "𝟘𝟙𝟚𝟛𝟜𝟝𝟞𝟟𝟠𝟡"
-composed = "ㇵㇶㇷㇸㇹㇻㇼㇽㇾㇿ"
+composed = "〰"
 
 # the sequence u type out.
 # "<M_>" means the compose key.
 # <MM> means compose key twice
 # ★ is what will be replaced with whats in regular
 # sequence = "<M_> ★|"
-sequence = "<M_> ★lh"
+sequence = "<M_> ★"
 
 
 
